3587ca2257212ba65709975c0ff26b28854a0bc1
ytasaka0407-glitch <ytasaka0407@gmail.com>
2026-02-19 16:09:56 +0900

    Update service-worker.js(バージョン変更)

--- FieldSurveyApp/service-worker.js
+++ FieldSurveyApp/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v2';
+const CACHE_VERSION = 'v3';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 
 const PRECACHE_URLS = [
@@ -68,3 +68,4 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
