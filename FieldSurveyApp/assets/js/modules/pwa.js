df88196df05f71ba38dc4e131f590823090bfb11
ytasaka0407-glitch <ytasaka0407@gmail.com>
2026-02-20 16:25:01 +0900

    Update pwa.js(ルートの変更)

--- FieldSurveyApp/assets/js/modules/pwa.js
+++ FieldSurveyApp/assets/js/modules/pwa.js
@@ -2,9 +2,8 @@
 export function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      // 修正点: 実ファイルの場所に合わせてパスを変更
       navigator.serviceWorker
-        .register('./FieldSurveyApp/service-worker.js')
+        .register('./service-worker.js') // ルートのSWを登録
         .then(reg => console.log('Service Worker registered:', reg.scope))
         .catch(err => console.error('Service Worker registration failed:', err));
     });
